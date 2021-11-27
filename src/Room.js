f74d6496837c81a89ee8810eb90ac6b8c626edc6
erineyes <nemesis.han@gmail.com>
2021-11-27 15:42:05 +0000

    update record demo

--- src/Room.js
+++ src/Room.js
@@ -1,4 +1,6 @@
 const config = require('./config')
+const recordclient = require('node-rest-client').Client;
+
 module.exports = class Room {
   constructor(room_id, worker, io) {
     this.id = room_id
@@ -15,6 +17,8 @@ module.exports = class Room {
 
     this.peers = new Map()
     this.io = io
+    this.client = new recordclient()
+    this.recorderIp = "3.37.52.70"
   }
 
   addPeer(peer) {
@@ -48,23 +52,22 @@ module.exports = class Room {
   }
   */
 
-  async publishRtpStream(socket_id, producer, rtpPort, rtcpPort) {
+  async publishRtpStream(socket_id, producer, recorderIp, rtpPort, rtcpPort) {
     const rtpTransport = await this.router.createPlainTransport(config.mediasoup.plainRtpTransport)
     console.log({
-	    ip: "13.124.39.3",
+	    ip: recorderIp,
 	    port: rtpPort,
     	    rtcpPort: rtcpPort
     })
 
     await rtpTransport.connect({
-	    ip: "13.124.39.3",
+	    ip: recorderIp,
 	    port: rtpPort,
     	    rtcpPort: rtcpPort
     })
 
-
     console.log(
-      "mediasoup VIDEO RTP SEND transport connected: %s:%d <--> %s:%d (%s)",
+      "mediasoup RTP SEND transport connected: %s:%d <--> %s:%d (%s)",
       rtpTransport.tuple.localIp,
       rtpTransport.tuple.localPort,
       rtpTransport.tuple.remoteIp,
@@ -73,7 +76,7 @@ module.exports = class Room {
     );
 
     console.log(
-      "mediasoup VIDEO RTCP SEND transport connected: %s:%d <--> %s:%d (%s)",
+      "mediasoup RTCP SEND transport connected: %s:%d <--> %s:%d (%s)",
       rtpTransport.rtcpTuple.localIp,
       rtpTransport.rtcpTuple.localPort,
       rtpTransport.rtcpTuple.remoteIp,
@@ -88,10 +91,11 @@ module.exports = class Room {
     const routerCodec = this.router.rtpCapabilities.codecs.find(
 	    codec => codec.kind === producer.kind
     )
- 
+    /* 
     console.log({
       'codecs': routerCodec 
     })
+    */
     codecs.push(routerCodec)
 
     const rtpCapabilities = {
@@ -104,12 +108,12 @@ module.exports = class Room {
 	    rtpCapabilities
     })
 
-    
     const rtpConsumer = await rtpTransport.consume({
 	    producerId: producer.id,
 	    rtpCapabilities
     })
 
+    /*
     console.log({
       'rtpConsumer': rtpConsumer
     })
@@ -134,11 +138,10 @@ module.exports = class Room {
       'rtpConsumer.rtpParameters.encodings': rtpConsumer.rtpParameters.encodings
     })
 
-
     console.log({
       'rtpConsumer.type': rtpConsumer.type
     })
-
+    */
     /*
     let { consumer, params } = await this.peers.get(socket_id).createConsumer(rtpTransport.id, producer.id, rtpCapabilities)
     consumer.on(
@@ -167,17 +170,114 @@ module.exports = class Room {
     }
   }
 
+
+  beginRecord(params) {
+    return new Promise(resolve => {
+      this.client.post("http://" + this.recorderIp + ":8080/StartRecord", params, (data,response)=> {
+        resolve(data)
+      })
+    })
+  }
+	
+  endRecord(params) {
+    return new Promise(resolve => {
+      this.client.post("http://" + this.recorderIp + ":8080/StopRecord", params, (data,response)=> {
+        resolve(data)
+      })
+    })
+  }
+
+  async stopRecord(socket_id) {
+        let params = {
+          headers: {
+            "Content-Type": "application/json"
+          },
+          data: {
+            "id": "test"
+          }
+        }
+
+        let res = await this.endRecord(params)
+        if (res.code == "success") {
+          console.log("stopping recording...")
+          //await this.unpublishRtpStream(socket_id, videoProducer, this.recorderIp, 50000, 50001)
+          console.log("stopped recording...")
+        } else {
+          console.log("--------")
+          console.log(res.reason)
+          console.log("--------")
+        }
+
+  }
+
   async startRecord(socket_id) {
     let producers = Array.from(this.peers.get(socket_id).producers.values())
-    //let params = {}
+    let useVideo = false
+    let useAudio = false
+    let videoProducer = null
+    let audioProducer = null
     for(const producer of producers) {
       try {
-        await this.publishRtpStream(socket_id, producer, 50000, 50001)
+	if (producer.kind == "video") {
+	  useVideo = true
+	  videoProducer = producer
+	} else if (producer.kind == "audio") {
+	  useAudio = true
+	  audioProducer = producer
+	}
+        //await this.publishRtpStream(socket_id, producer, 50000, 50001)
       } catch (error) {
         console.log(error)
       }
     }
-    //return params
+    
+    try {
+      if (useVideo && useAudio) {
+
+
+      } else if (useVideo) {
+
+	let params = {
+	  headers: { 
+	    "Content-Type": "application/json"
+	  },
+	  data: {
+	    "id": "test",
+	    "name": "testName",
+	    "path": "/home/centos/workspace/pulsar/out",
+	    "video": {
+              "codec": "VP8",
+              "rtp_port": 50000,
+              "rtcp_port": 50001
+            }
+	  }
+	}
+
+	let res = await this.beginRecord(params) 
+	if (res.code == "success") {
+          console.log("starting recording...")
+          await this.publishRtpStream(socket_id, videoProducer, this.recorderIp, 50000, 50001)
+	  console.log("started recording...")
+	} else {
+	  console.log("--------")
+	  console.log(res.reason)
+	  console.log("--------")
+	}
+
+	
+
+      } else if (useAudio) {
+
+
+      } else {
+
+
+      }
+
+    } catch (error) {
+      console.log(error)
+    }
+     
   }
 
   async createWebRtcTransport(socket_id) {
